a137167f5b00d9fea1630d382cae49b0bd3cc40e
Caylin James <cjames@leapagency.com>
2023-08-01 11:55:53 -0400

    create separate App component, set up basic props

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('h2', {}, props.animal),
+        React.createElement('h2', {}, props.breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet, {
+            animal: 'Dog',
+            name: 'Gerat',
+            breed: 'Italian Greyhound',
+        }),
+        React.createElement(Pet, {
+            animal: 'Bird',
+            name: 'Pepper',
+            breed: 'Cockatiel',
+        }),
+        React.createElement(Pet, {
+            animal: 'Cat',
+            name: 'Zephyr',
+            breed: 'Russian Blue',
+        }),
+    ]);
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
