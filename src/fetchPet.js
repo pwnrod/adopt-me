c09832bea97a58d18cdce6df11d70ded1cca7fd0
Caylin James <caylinjames.dev@gmail.com>
2023-08-02 20:18:16 -0400

    implement react query (awesome)

--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,5 +1,5 @@
 const fetchPet = async ({ queryKey }) => {
-    const id = queryKey(1);
+    const id = queryKey[1];
 
     const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
 
